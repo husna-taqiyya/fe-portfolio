881b6a16470f63ff7993d5ef4c6346491055eb48
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-16 09:27:02 +0700

    buat layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="">
-        <div class="font-bold text-red-500 text-xl">
-            ini index
+    <div class="min-h-screen flex">
+        <div class="bg-slate-600 min-h-screen grow grid grid-cols-10">
+            <div class="cols-span-10 md:col-span-3 bg-orange-200">
+                <div class="fixed">content 1 FIXED ketika medium</div>
+            </div>
+            <div class="cols-span-10 md:col-span-7 bg-orange-600">
+                <div class="min-h-screen bg-lime-200">Section 1</div>
+                <div class="min-h-screen bg-lime-400">Section 2</div>
+                <div class="min-h-screen bg-lime-600">Section 3</div>
+                <div class="min-h-screen bg-lime-800">Section 3</div>
+            </div>
         </div>
-        <button class="btn btn-primary">Tombol</button>
-        <button class="btn btn-secondary">Tombol</button>
-        <button class="btn btn-accent">Tombol</button>
-        <button class="btn btn-neutral">Tombol</button>
-        <button class="btn btn-base-100">Tombol</button>
-        <button class="btn btn-info">Tombol</button>
-        <button class="btn btn-success">Tombol</button>
-        <button class="btn btn-warning">Tombol</button>
-        <button class="btn btn-error">Tombol</button>
+        <div class="bg-slate-300 min-h-screen w-28 flex-none max-md:hidden">
+            <div class="fixed">ini sidebar, muncul ketika medium</div>
+        </div>
     </div>
 </template>
 
