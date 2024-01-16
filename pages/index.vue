3970a33b79a5d9a67c8ee4c2d2ff7621184fc46b
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-16 09:07:45 +0700

    percobaan button

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-    <div class="font-bold text-red-500 text-xl">
-        ini index
+    <div class="">
+        <div class="font-bold text-red-500 text-xl">
+            ini index
+        </div>
+        <button class="btn btn-primary">Tombol</button>
+        <button class="btn btn-secondary">Tombol</button>
+        <button class="btn btn-accent">Tombol</button>
+        <button class="btn btn-neutral">Tombol</button>
+        <button class="btn btn-base-100">Tombol</button>
+        <button class="btn btn-info">Tombol</button>
+        <button class="btn btn-success">Tombol</button>
+        <button class="btn btn-warning">Tombol</button>
+        <button class="btn btn-error">Tombol</button>
     </div>
 </template>
 
