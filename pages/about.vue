955874aa240b8926cadc900be8ca6075c35e9bf2
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-16 08:48:41 +0700

    install tailwind

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        PROJECT
+        ini halaman about
     </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,9 @@
 <template>
-    <h1>Ninja Reaction Timer</h1>
-    <button @click="start">play</button>
-    <Block />
+    <div class="font-bold text-red-500 text-xl">
+        ini index
+    </div>
 </template>
 
-<script>
-import Block from '../pages/block.vue'
-export default {
-    name: 'App',
-    components: { Block },
-    data() {
-        return {
-            isPlaying: false,
-            delay: null
-        }
-    },
-    methods: {
-        start() {
-            this.delay = 2000 + Math.random() * 5000
-            this.isPlaying = true
-        }
-    }
-}
-</script>
+<script setup>
 
-<style>
-#app {
-    font-family: Avenir, Arial, Helvetica, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #444;
-    margin-top: 60px;
-}
-</style>
+</script>
