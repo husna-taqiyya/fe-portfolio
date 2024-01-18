f73fc2c97a2ad3af276c5d98b62288951d103b21
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-18 10:59:26 +0700

    update resume

--- components/index/resume.vue
+++ components/index/resume.vue
@@ -1,13 +1,37 @@
 <template>
-    <div class="min-h-screen" id="resume">
-        <div>Resume</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus perspiciatis corrupti sint, commodi
-            doloribus similique. Eaque aperiam autem numquam voluptas, culpa ipsa fugit laboriosam aspernatur ea ut.
-            Magnam explicabo libero a ratione unde consequuntur numquam, totam fuga dolores laborum id reprehenderit,
-            obcaecati officia, facilis molestiae vel accusantium aut eligendi nam.</div>
+    <div class="min-h-screen py-12" id="resume">
+        <div class="rounded-2xl border border-neutral w-fit px-4 py-1 flex items-center gap-2">
+            <LucideBriefcase :size="16" />
+            <div class="uppercase text-sm font-light">Resume</div>
+        </div>
+        <div class="flex flex-col gap-6 mt-10">
+            <div class="text-4xl font-semibold">Education & Experience</div>
+            <div>
+                <!-- list experience -->
+                <div class="flex gap-12" v-for="n in 2" key="n">
+                    <div class="flex flex-col items-center">
+                        <LucideCircle :size="12" class="fill-neutral stroke-none" />
+                        <div class="grow w-px bg-neutral"></div>
+                    </div>
+                    <div class="mb-10">
+                        <div>2020 - Present</div>
+                        <div class="flex flex-col gap-4">
+                            <div>
+                                <div class="text-4xl font-semibold mt-5">Framer Desinger & Developer</div>
+                                <div class="text-light">Brunodee Agency</div>
+                            </div>
+                            <div>
+                                <div class="text-4xl font-semibold">Front-End WordPress Developer</div>
+                                <div class="text-light">Envato Market</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
+<script setup>
 
 </script>
