6fbabc5f89cb45bb2972721cd9d24a48bf7a5275
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-18 10:09:10 +0700

    update about

--- components/index/about.vue
+++ components/index/about.vue
@@ -1,10 +1,21 @@
 <template>
-    <div class="min-h-screen" id="about">
-        <div>About</div>
-        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas optio corrupti, facilis dolores modi sint
-            eaque quos ex recusandae provident molestiae dicta amet nostrum magni quis laborum deleniti inventore
-            repellendus? Eius ipsa magni error aspernatur tempore! Accusamus quia cumque autem esse facilis odit illum
-            nobis? Iusto dolor harum accusamus perferendis!</div>
+    <div class="min-h-screen py-12" id="about">
+        <div class="rounded-2xl border border-neutral w-fit px-4 py-1 flex items-center gap-2">
+            <div>
+                <LucideUserRound :size="16" />
+            </div>
+            <div>About</div>
+        </div>
+        <div class="flex flex-col gap-6 mt-10">
+            <div class="text-4xl font-semibold">Every great design begin with an even better story</div>
+            <div class="font-light">Since beginning my journey as a freelance designer nearly 8 years ago, I've done remote
+                work for agencies,
+                consulted for startups, and collaborated with talented people to create digital products for both business
+                and
+                consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chopsone
+                design
+                problem at a time.</div>
+        </div>
     </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="grid grid-cols-10">
         <!-- PROFILE CARD -->
-        <div class="cols-span-10 md:col-span-5 lg:col-span-4 xl:col-span-3 min-h-screen p-6">
+        <div class="col-span-10 md:col-span-5 lg:col-span-4 xl:col-span-3 min-h-screen p-6">
             <IndexProfilecard />
         </div>
         <!-- SECTIONS -->
-        <div class="cols-span-10 md:col-span-5 lg:col-span-6 xl:col-span-7 ">
+        <div class="col-span-10 md:col-span-5 lg:col-span-6 xl:col-span-7 p-6 lg:px-20 xl:px-40">
             <IndexAbout />
             <IndexResume />
             <IndexProject />
