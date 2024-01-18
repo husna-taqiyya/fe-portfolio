12baca36fcdd37813d72ab116ad8fc57d717d7e9
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-18 11:33:49 +0700

    update blog text

--- components/index/blog.vue
+++ components/index/blog.vue
@@ -5,15 +5,22 @@
             <div class="uppercase text-sm font-light">Blog</div>
         </div>
         <div class="flex flex-col gap-6 mt-10">
+            <div class="text-4xl font-semibold">Latest Blog</div>
             <!-- LIST BLOG -->
             <div class="grid grid-cols-10 gap-3" v-for="n in 4" :key="n">
                 <!-- IMAGES -->
-                <div class="aspect-video bg-neutral col-span-3 rounded-lg"></div>
-                <div class="col-span-7">
+                <div class="aspect-video bg-neutral col-span-4 xl:col-span-3 rounded-lg"></div>
+                <div class="col-span-6 xl:col-span-7">
                     <div class="text-2xl font-semibold">Judul Blog</div>
-                    <div class="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nostrum similique
-                        natus. Maxime
-                        cupiditate ipsam, dignissimos quia corrupti sapiente molestias.</div>
+                    <div class="font-light line-clamp-2 xl:line-clamp-3">Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Incidunt inventore corporis iste eveniet error quisquam voluptatem sed tempora enim officia
+                        numquam, cupiditate ad, a dolorem autem harum sequi. Minus at dolor possimus consectetur sint et
+                        alias, expedita sapiente aut delectus porro ut fuga itaque temporibus magni non quod. Hic, similique
+                        molestiae ducimus ipsum, esse minus distinctio saepe voluptate aperiam accusantium ea odit quis
+                        consequuntur aliquam corporis. Dicta, atque nulla itaque ipsum quos dolor vero voluptatibus mollitia
+                        excepturi maiores quisquam corporis fugiat voluptates repellendus minima officiis laborum commodi
+                        expedita explicabo tempore neque fuga vitae? Ut, eveniet ratione? Consequatur, odio asperiores.
+                        Harum?</div>
                 </div>
             </div>
         </div>
