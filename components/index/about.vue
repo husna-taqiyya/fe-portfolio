6deafbe4cd31128e0e7ee556580770069f6afebe
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-19 10:41:38 +0700

    update about

--- components/index/about.vue
+++ components/index/about.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="min-h-screen py-12" id="about">
+    <div class="min-h-screen py-12 flex flex-col" id="about">
         <div class="rounded-2xl border border-neutral w-fit px-4 py-1 flex items-center gap-2">
             <div>
                 <LucideUserRound :size="16" />
             </div>
             <div>About</div>
         </div>
-        <div class="flex flex-col gap-6 mt-10">
-            <div class="text-4xl font-semibold">Every great design begin with an even better story</div>
+        <div class="flex flex-col gap-12 mt-10 grow">
+            <div class="text-6xl leading-tight">Say Hi from <span class="text-accent">Drake</span>, Webflow Designer and
+                Developer</div>
+            <!-- <div class="text-4xl font-semibold">Every great design begin with an even better story</div> -->
             <div class="font-light">Since beginning my journey as a freelance designer nearly 8 years ago, I've done remote
                 work for agencies,
                 consulted for startups, and collaborated with talented people to create digital products for both business
@@ -15,6 +17,16 @@
                 consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chopsone
                 design
                 problem at a time.</div>
+        </div>
+        <div class="flex items-end gap-20 grow pb-20">
+            <div>
+                <div class="text-4xl text-accent font-semibold">10+</div>
+                <div>YEARS OF <br> EXPERIENCE</div>
+            </div>
+            <div>
+                <div class="text-4xl text-accent font-semibold">10+</div>
+                <div>PROJECT <br> COMPLETED</div>
+            </div>
         </div>
     </div>
 </template>
