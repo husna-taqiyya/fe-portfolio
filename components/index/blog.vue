8c43e1d25e26acb3db0ed78bbb9d174eb8312b38
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-19 10:06:00 +0700

    add animation to blog

--- components/index/blog.vue
+++ components/index/blog.vue
@@ -7,11 +7,13 @@
         <div class="flex flex-col gap-6 mt-10">
             <div class="text-4xl font-semibold">Latest Blog</div>
             <!-- LIST BLOG -->
-            <div class="grid grid-cols-10 gap-3" v-for="n in 4" :key="n">
+            <div class="grid grid-cols-10 gap-3 group hover:bg-secondary p-2 rounded-xl" v-for="n in 4" :key="n">
                 <!-- IMAGES -->
-                <div class="aspect-video bg-neutral col-span-4 xl:col-span-3 rounded-lg"></div>
+                <div class="aspect-video bg-neutral col-span-4 xl:col-span-3 rounded-lg group-hover:scale-105 duration-300">
+                </div>
                 <div class="col-span-6 xl:col-span-7">
-                    <div class="text-2xl font-semibold">Judul Blog</div>
+                    <!-- judul -->
+                    <div class="text-2xl font-semibold group-hover:text-accent">Judul Blog</div>
                     <div class="font-light line-clamp-2 xl:line-clamp-3">Lorem, ipsum dolor sit amet consectetur adipisicing
                         elit. Incidunt inventore corporis iste eveniet error quisquam voluptatem sed tempora enim officia
                         numquam, cupiditate ad, a dolorem autem harum sequi. Minus at dolor possimus consectetur sint et
