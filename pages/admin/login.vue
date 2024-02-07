f0bff841177c508d0405c20faaf7bf977d730882
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-02-07 11:47:43 +0700

    handle login

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,29 +1,32 @@
 <template>
+    <!-- max screen h overflow supaya tidak ada scrolling atas bawah dan kanan kiri -->
     <div class="w-screen h-screen max-h-screen max-w-screen overflow-hidden bg-white">
         <!-- max lg -> rows 2 -->
         <!-- min lg -> cols 2 -->
-        <div class="h-full w-full grid max-lg:grid-rows-2 lg:grid-cols-2">
+        <div class="h-full w-full grid max-lg:grid-rows-10 lg:grid-cols-2">
             <div class="max-lg:row-span-4 flex flex-col justify-center items-center">
                 <ImagesProgrammer class="w-[125%]" />
             </div>
-            <div class="max-lg:row-span-6 bg-[#EDD6D6] flex flex-col gap-12 justify-center items-center px-10 lg:px-32">
+            <div
+                class="max-lg:row-span-6 bg-[#EDD6D6] flex flex-col gap-6 lg:gap-12 justify-center items-center px-10 lg:px-32">
                 <!-- nama -->
-                <div class="font-bungee-shade text-nowrap text-3xl md:text-6xl xl:text-6xl">{{ fullname }}</div>
+                <div class="font-bungee-shade text-nowrap text-3xl md:text-6xl lg:text-4xl xl:text-6xl">{{ fullname }}</div>
 
                 <!-- input email -->
                 <div class="w-full font-arc-daughter text-xl md:text-2xl">
                     <label>Email</label>
-                    <input type="text" placeholder="Email" class="input w-full bg-[#D3C7C7]">
+                    <input v-model="formData.email" type="text" placeholder="Email" class="input w-full bg-[#D3C7C7]">
                 </div>
 
                 <!-- input password -->
                 <div class="w-full font-arc-daughter text-xl md:text-2xl">
                     <label>Password</label>
-                    <input type="text" placeholder="Password" class="input w-full bg-[#D3C7C7]">
+                    <input v-model="formData.password" type="password" placeholder="Password"
+                        class="input w-full bg-[#D3C7C7]">
                 </div>
 
                 <!-- login buttom -->
-                <button
+                <button @click="doLogin"
                     class="font-baloo-bhai btn border-0 text-xl md:text-2xl bg-[#D3C7C7] p-10 md:px-20 py-0 lg:px-32 md:py-2 h-min text-nowrap">LOGIN
                     NOW</button>
             </div>
@@ -39,4 +42,29 @@ definePageMeta({
 
 const { value: useProfile } = useState('profile');
 const fullname = `${useProfile.firstname} ${useProfile.lastname}`;
+
+const formData = ref({
+    email: '',
+    password: ''
+});
+
+const config = useRuntimeConfig();
+const apiUri = config.public.apiUri;
+const doLogin = async () => {
+    // convert data to json
+    const jsonData = JSON.stringify(formData.value);
+
+    const response = await $fetch(apiUri + '/login', {
+        method: 'POST',
+        body: jsonData,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    console.log(response)
+
+    // sementara redirect ke halaman home
+    navigateTo('/');
+}
+
 </script>
