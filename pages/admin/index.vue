14aeb5aa23ad4dd4407c72cdde642953e35ebaf0
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-02-08 10:09:22 +0700

    add admin mobile header

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,12 +1,23 @@
 <template>
     <div data-theme:="light" class="w-screen h-screen max-w-screen min-h-screen bg-gray-300">
-        <!-- HEADER -->
-        <div class="h-[70px] z-[51] relative border-white/[0.08] px-3 sm:px-8 md:py-0 mb-12">
+        <!-- MOBILE HEADER -->
+        <div class="lg:hidden h-[70px] relative border-b border-white/20 px-3">
+            <div class="h-full flex justify-between items-center">
+                <a href="" class="flex-none flex gap-4 items-center pr-4">
+                    <ImagesWings class="h-8" />
+                </a>
+
+                <LucideAlignRight :size="24" />
+            </div>
+        </div>
+
+        <!-- MAIN HEADER -->
+        <div class="h-[70px] z-[51] relative border-b border-white/[0.08] px-3 sm:px-8 md:py-0 mb-12">
             <div class="h-full flex items-center divide-x divide-white/20">
 
                 <!-- SISI KIRI -->
                 <a class="flex flex-none gap-4 items-center pr-4">
-                    <ImagesMonas class="h-8" />
+                    <ImagesWings class="h-8" />
                     <div>HUSNA TAQIYYA</div>
                 </a>
 
