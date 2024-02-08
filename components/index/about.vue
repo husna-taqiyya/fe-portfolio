0c5e32606d8afb6442c7731a52fca3e88abd944e
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-02-08 08:44:52 +0700

    about job

--- components/index/about.vue
+++ components/index/about.vue
@@ -10,7 +10,10 @@
             <div class="text-6xl leading-tight">Say Hi from <span class="text-accent">{{ profile.firstname }}</span>,{{
                 profile.job }}</div>
             <!-- <div class="text-4xl font-semibold">Every great design begin with an even better story</div> -->
-            <div class="font-light">{{ profile.job }}</div>
+            <div class="font-light">Since beginning my journey as a freelance designer nearly 8 years ago, I've done remote
+                work for agencies, consulted for startups, and collaborated with talented people to create digital products
+                for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on
+                improving my chopsone design problem at a time.</div>
         </div>
         <div class="flex items-end gap-20 grow pb-20">
             <div>
